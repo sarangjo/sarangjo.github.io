9b8f97044acc8be06bde2a394be26aa467625dc8
Sarang Joshi <sarangj@msn.com>
2018-05-25 03:07:29 -0700

    Really just wasting time

--- script.js
+++ script.js
@@ -1,57 +1,39 @@
-var classes = {
-  '15sp': 143,
-  '15au': 143,
-  '16wi': 351,
-  '16sp': 351,
-  '16au': 311,
-  '17wi': 351,
-  '17sp': 451,
-  '17au': 451,
-  '18wi': 451,
-  '18sp': 452,
-};
-
-var classes2 = {
-  '143': ['15sp', '15au'],
-  '351': ['16wi', '16sp', '17wi'],
-  '311': ['16au'],
-  '451': ['17sp', '17au', '18wi'],
-  '452': ['18sp']
-};
-
-if (true) {
-  var list = document.getElementById("ta2");
-  for (var c in classes2) {
-    var el = document.createElement("li");
-    var cCell = document.createElement("span");
-    cCell.textContent = `${c}: `;
-    el.appendChild(cCell);
-
-    var quarters = classes2[c];
-    for (var i = 0; i < quarters.length; i++) {
-      var a = document.createElement("a");
-      a.setAttribute('href', 'https://courses.cs.washington.edu/courses/cse' + c + '/' + quarters[i] + '/');
-      a.setAttribute('target', 'blank');
-      a.textContent = quarters[i];
-      el.appendChild(a);
-      el.innerHTML += ' ';
-    }
-    list.appendChild(el);
+var classes = [
+  {
+    'num': '143',
+    'quarters': ['15sp', '15au']
+  }, {
+    'num': '351',
+    'quarters': ['16wi', '16sp', '17wi']
+  }, {
+    'num': '311',
+    'quarters': ['16au']
+  }, {
+    'num': '451',
+    'quarters': ['17sp', '17au', '18wi']
+  }, {
+    'num': '452',
+    'quarters': ['18sp']
   }
-} else {
-  var table = document.getElementById("ta");
-  for (var q in classes) {
-    var row = document.createElement("tr");
-    var qCell = document.createElement("td");
-    qCell.textContent = q;
-    row.appendChild(qCell);
-    var cCell = document.createElement("td");
+];
+
+var list = document.getElementById("ta");
+for (var i = 0; i < classes.length; i++) {
+  var classData = classes[i];
+
+  var el = document.createElement("li");
+  var cCell = document.createElement("span");
+  cCell.textContent = `${classData.num}: `;
+  el.appendChild(cCell);
+
+  var quarters = classData.quarters;
+  for (var j = 0; j < quarters.length; j++) {
     var a = document.createElement("a");
-    a.setAttribute('href', 'https://courses.cs.washington.edu/courses/cse' + classes[q] + '/' + q + '/');
+    a.setAttribute('href', 'https://courses.cs.washington.edu/courses/cse' + classData.num + '/' + quarters[j] + '/');
     a.setAttribute('target', 'blank');
-    a.textContent = classes[q];
-    cCell.appendChild(a);
-    row.appendChild(cCell);
-    table.appendChild(row);
+    a.textContent = quarters[j];
+    el.appendChild(a);
+    el.innerHTML += ' ';
   }
+  list.appendChild(el);
 }
