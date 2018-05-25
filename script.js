df0d022ebf5c5e42b530cc9d7733a35eb8f2ebb2
Sarang Joshi <sarangj@msn.com>
2018-05-25 03:01:24 -0700

    Reformatting

--- script.js
+++ script.js
@@ -11,18 +11,47 @@ var classes = {
   '18sp': 452,
 };
 
-var table = document.getElementById("ta");
-for (var q in classes) {
-  var row = document.createElement("tr");
-  var qCell = document.createElement("td");
-  qCell.textContent = q;
-  row.appendChild(qCell);
-  var cCell = document.createElement("td");
-  var a = document.createElement("a");
-  a.setAttribute('href', 'https://courses.cs.washington.edu/courses/cse' + classes[q] + '/' + q + '/');
-  a.setAttribute('target', 'blank');
-  a.textContent = classes[q];
-  cCell.appendChild(a);
-  row.appendChild(cCell);
-  table.appendChild(row);
+var classes2 = {
+  '143': ['15sp', '15au'],
+  '351': ['16wi', '16sp', '17wi'],
+  '311': ['16au'],
+  '451': ['17sp', '17au', '18wi'],
+  '452': ['18sp']
+};
+
+if (true) {
+  var list = document.getElementById("ta2");
+  for (var c in classes2) {
+    var el = document.createElement("li");
+    var cCell = document.createElement("span");
+    cCell.textContent = `${c}: `;
+    el.appendChild(cCell);
+
+    var quarters = classes2[c];
+    for (var i = 0; i < quarters.length; i++) {
+      var a = document.createElement("a");
+      a.setAttribute('href', 'https://courses.cs.washington.edu/courses/cse' + c + '/' + quarters[i] + '/');
+      a.setAttribute('target', 'blank');
+      a.textContent = quarters[i];
+      el.appendChild(a);
+      el.innerHTML += ' ';
+    }
+    list.appendChild(el);
+  }
+} else {
+  var table = document.getElementById("ta");
+  for (var q in classes) {
+    var row = document.createElement("tr");
+    var qCell = document.createElement("td");
+    qCell.textContent = q;
+    row.appendChild(qCell);
+    var cCell = document.createElement("td");
+    var a = document.createElement("a");
+    a.setAttribute('href', 'https://courses.cs.washington.edu/courses/cse' + classes[q] + '/' + q + '/');
+    a.setAttribute('target', 'blank');
+    a.textContent = classes[q];
+    cCell.appendChild(a);
+    row.appendChild(cCell);
+    table.appendChild(row);
+  }
 }
