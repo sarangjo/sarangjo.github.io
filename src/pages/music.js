7882b5a8377c4f9c1766412ea128a1a3c26419e1
Sarang Joshi <sarangj@msn.com>
2020-01-18 05:36:26 -0800

    Add content to music

--- src/pages/music.js
+++ src/pages/music.js
@@ -6,19 +6,6 @@ import { L } from "../utils";
 
 import "./projects.css";
 
-const labelToTitle = type => {
-  switch (type) {
-  case "uw":
-    return "UW";
-  case "asa":
-    return "ASA";
-  case "kinspire":
-    return "Kinspire";
-  default:
-    return "";
-  }
-};
-
 const Label = ({ type }) => (
   <span className={`label ${type}`}>{labelToTitle(type)}</span>
 );
@@ -27,101 +14,6 @@ Label.propTypes = {
 };
 
 class ProjectsPage extends React.Component {
-  completed() {
-    return (
-      <>
-        <h3>Completed Projects</h3>
-        <ul>
-          <li>
-            Full-stack single-page Typescript app (
-            <L to="https://github.com/sarangjo/full-stack-typescript-app">
-              source code
-            </L>
-            )
-          </li>
-          <li>
-            <L to="https://sarangjo.github.io/cse351-heap/">
-              CSE 351 Heap Simulator
-            </L>
-          </li>
-          <li>
-            Paper Telephone (
-            <L to="https://github.com/sarangjo/paper-telephone">source code</L>)
-            <Label type="uw" />
-          </li>
-          <li>
-            <L to="https://kinspire.org/">Kinspire Website</L>
-            <Label type="kinspire" />
-          </li>
-          <li>
-            <L to="https://chrome.google.com/webstore/detail/uw-awaaz-transposer/fjhhmdelhgjdjklocpgdoljjkneebcog">
-              Transposer for UW Awaaz sheet music
-            </L>{" "}
-            (<L to="https://github.com/sarangjo/awaaz-parser">source code</L>)
-            <Label type="uw" />
-          </li>
-          <li>
-            ASA Score Checker and Reporter (
-            <L to="https://github.com/sarangjo/asa-judging">source code</L>)
-            <Label type="asa" />
-          </li>
-          <li>
-            <L to="https://sarangjo.github.io/formations/public/">Formations</L>
-          </li>
-          <li>Doc Scheduler</li>
-          <li>LightRunner</li>
-          <li>Radio Transceiver</li>
-        </ul>
-        <h3>From My Early Days</h3>
-        <ul>
-          <li>
-            YahtzeePC (
-            <L to="https://github.com/sarangjo/yahtzeepc/">source code</L>)
-          </li>
-          <li>LOGO projects</li>
-          <li>IonoWumpus</li>
-        </ul>
-      </>
-    );
-  }
-
-  inProgress() {
-    return (
-      <>
-        {" "}
-        <h3>In progress</h3>
-        <ul>
-          <li>
-            <L to="https://sarangjo.github.io/rlesports-viz">
-              Rocket League eSports visualization
-            </L>{" "}
-            (<L to="https://github.com/sarangjo/rlesports-viz">source code</L>)
-          </li>
-          <li>
-            Kinspire Portal (
-            <L to="https://github.com/kinspire/kinspire-portal">source code</L>)
-            <Label type="kinspire" />
-          </li>
-          <li>
-            <L to="https://ekta.app/">Ekta: The Desi Common Portal</L>
-            <Label type="asa" />
-          </li>
-          <li>SS Soundboard</li>
-          <li>Couch game simulation</li>
-          <li>When2meet for mobile devices</li>
-          <li>Poll Everywhere notifier</li>
-          <li>
-            UW CSE Courses visualization <span className="label uw">UW</span>
-          </li>
-          <li>
-            UW Calendar (who knows when this will actually be completed){" "}
-            <Label type="uw" />
-          </li>
-        </ul>
-      </>
-    );
-  }
-
   render() {
     return (
       <Layout
@@ -129,23 +21,50 @@ class ProjectsPage extends React.Component {
         title="Projects"
         description="What else would companies look at to hire me?"
       >
-        <h2>
-          Projects{" "}
-          <L to="https://github.com/sarangjo">
-            <img className="github-logo" src="/images/github.png" />
-          </L>
-        </h2>
-        {this.completed()}
-        {this.inProgress()}
-        <h2>Open source contributions</h2>
+        <h2>Music</h2>
+        <h3>High School and Before</h3>
         <ul>
           <li>
-            Gatsby plugin{" "}
-            <L to="https://www.npmjs.com/package/gatsby-transformer-fountain">
-              gatsby-transformer-fountain
+            <L to="https://www.youtube.com/watch?v=gjjT9nfSU5Y&list=PLB0785C00EA91F358&index=2">
+              Gaaravaa (गारवा)
+            </L>
+          </li>
+          <li>
+            <L to="https://soundcloud.com/sarang-joshi/tu-hi-hakeekat">
+              Tu Hi Hakeekat (तू ही हकीकत)
+            </L>
+          </li>
+          <li>
+            <L to="https://soundcloud.com/sarang-joshi/musafir-chalte-chalte">
+              Musafir Chalte Chalte (मुसाफिर चलते चलते)
+            </L>
+          </li>
+          <li>
+            <L to="https://www.youtube.com/watch?v=KQERT1iXzg4&list=PLB0785C00EA91F358&index=3">
+              Tya Phulanchya Gandhakoshi (त्या फुलांच्या गंधकोशी)
+            </L>
+          </li>
+          <li>
+            <L to="https://www.youtube.com/watch?v=wMnfi-He0Uc&list=PLB0785C00EA91F358&index=4">
+              Farewell
+            </L>{" "}
+            circa 2011
+          </li>
+          <li>
+            <L to="https://www.youtube.com/watch?v=oZjD6p00NQA&list=PLB0785C00EA91F358&index=5">
+              Part of a Painting, <i>Edges</i>
+            </L>{" "}
+            circa 2012. Nominated for "Outstanding Performance by an Actor in a
+            Featured Ensemble Role" at the{" "}
+            <L to="https://www.5thavenue.org/images/pdfs/1213_5thAveAwards_Nominees_and_Recipients.pdf">
+              2013 5th Avenue Awards
             </L>
           </li>
         </ul>
+        <h3>Undergrad</h3>
+        Most of my music was with UW Awaaz, here are some of my favorites.
+        <h3>After Undergrad</h3>
+        <h3>The Joshi Show</h3>
       </Layout>
     );
   }
