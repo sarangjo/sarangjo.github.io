0f7177ee52495e056d28880a35413da49244da03
Sarang Joshi <sarangj@msn.com>
2020-01-19 03:11:28 -0800

    Improve homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,16 @@ import PropTypes from "prop-types";
 
 import Layout from "../components/layout";
 
-const LINKS = [
-  "https://illumio.com",
-  "https://uw.edu",
-  "https://cs.uw.edu",
-  "https://www.youtube.com/playlist?list=PLB0785C00EA91F358",
-  "https://www.youtube.com/playlist?list=PLHGainiu-yeQDDL-MlGiJxLC_ayim-aEg",
-  "https://github.com/sarangjo",
-];
+const Links = {
+  illumio: "https://illumio.com",
+  uw: "https://uw.edu",
+  cs: "https://cs.uw.edu",
+  songs: "https://www.youtube.com/playlist?list=PLB0785C00EA91F358",
+  games:
+    "https://www.youtube.com/playlist?list=PLHGainiu-yeQDDL-MlGiJxLC_ayim-aEg",
+  kinspire: "https://kinspire.org",
+  asa: "https://desiacappella.org",
+};
 
 class Index extends React.Component {
   render() {
@@ -32,17 +34,26 @@ class Index extends React.Component {
       >
         <p>
           Hi there! I&apos;m Sarang Joshi, a software developer, singer, and
-          life enthusiast working at <L to={LINKS[0]}>Illumio</L> in Bellevue,
-          WA. I graduated cum laude from the{" "}
-          <L to={LINKS[1]}>University of Washington, Seattle</L>, with a B.S. in{" "}
-          <L to={LINKS[2]}>Computer Science</L> with Honors.
+          life enthusiast working at <L to={Links.illumio}>Illumio</L> in
+          Bellevue, WA. I graduated cum laude from the{" "}
+          <L to={Links.uw}>University of Washington, Seattle</L>, with a B.S. in{" "}
+          <L to={Links.cs}>Computer Science</L> with Honors.
         </p>
         <p>
-          Outside of work, I like to <L to={LINKS[3]}>sing</L>,{" "}
-          <Link to="/blog">write</Link>, <L to={LINKS[4]}>play video games</L>,{" "}
-          <Link to="/projects">program</Link>, and travel when I can.
-          You&apos;ll find most of my work on this website, so feel free to
-          explore!
+          I am also the Co-Director of <L to={Links.kinspire}>Kinspire</L>, a
+          501(c)(3) dedicated to the revitalization of education for children in
+          orphanages in India, and Co-Director of{" "}
+          <L to={Links.asa}>the Association of South-Asian A cappella</L>, a
+          non-profit working to foster the growth of the South-Asian a cappella
+          artform in college campuses and across the country.
+        </p>
+        <p>
+          Outside of that, I like to <L to={Links.songs}>sing</L>,{" "}
+          <Link to="/blog">write</Link>,{" "}
+          <L to={Links.games}>play video games</L>,{" "}
+          <Link to="/projects">program even more</Link>, and travel when I can.
+          This website is intended to be a central place to keep track of all of
+          my work, so feel free to explore!
         </p>
       </Layout>
     );
